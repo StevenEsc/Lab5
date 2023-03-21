e155573fb9ff073970ab57a5dc0c8b7713a7c433
StevenEsc <005989655@coyote.csusb.edu>
2023-03-20 20:45:43 -0700

    This one has the second function

--- Version_control.py
+++ Version_control.py
@@ -4,3 +4,11 @@ def even():
          print("Number is even")
 
 even()
+
+def odd ():
+    num = int(input("Please enter a number : "))
+    if num % 2 !=0:
+        print ("This number is odd")
+    elif num  % 2 == 0:
+        print ("this number is even")
+odd()
